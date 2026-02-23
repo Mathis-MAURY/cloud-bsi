3884c8fa1aaf229f4e95a51f3193bb9ad22ca780
Mathis_Maury <122210817+Mathis-MAURY@users.noreply.github.com>
2026-02-23 23:10:02 +0100

    Update admin_password variable definition
    Refactor admin_password variable to include description, type, and sensitive attributes.

--- variables.tf
+++ variables.tf
@@ -2,4 +2,8 @@ variable "prefix" { default = "tp-mathis" }
 variable "location" { default = "France Central" }
 variable "vm_size" { default = "Standard_B1s" }
 variable "admin_username" { default = "azureuser" }
-variable "admin_password" { default = "P@ssw0rd_TP_Note_2026!" }
+variable "admin_password" {
+  description = "Mot de passe administrateur"
+  type        = string
+  sensitive   = true  # <--- Cache le mot de passe dans les logs du terminal
+}
